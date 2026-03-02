4d1a23a3a7581e31106b61e3174d1877626d04e9
RamirezMarquesDev <ramirezmarquescampello@gmail.com>
2026-03-02 20:54:53 -0300

    wilhe logic

--- exercicios/aula42.py
+++ exercicios/aula42.py
@@ -1,26 +1,37 @@
-frase = 'aaaooo'
+# frase = 'aaaooo'
 
-i = 0
-qtd_apareceu_mais_vezes = 0
-letra_apareceu_mais_vezes = ''
+# i = 0
+# qtd_apareceu_mais_vezes = 0
+# letra_apareceu_mais_vezes = ''
 
-while i < len(frase):
-    letra_atual = frase[i]
+# while i < len(frase):
+#     letra_atual = frase[i]
 
-    if letra_atual == ' ':
-        i += 1
-        continue
+#     if letra_atual == ' ':
+#         i += 1
+#         continue
 
-    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
+#     qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
 
-    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
-        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
-        letra_apareceu_mais_vezes = letra_atual
+#     if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
+#         qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+#         letra_apareceu_mais_vezes = letra_atual
 
-    i += 1
+#     i += 1
 
-print(
-    'A letra que apareceu mais vezes foi '
-    f'"{letra_apareceu_mais_vezes}" que apareceu '
-    f'{qtd_apareceu_mais_vezes}x'
-)
+# print(
+#     'A letra que apareceu mais vezes foi '
+#     f'"{letra_apareceu_mais_vezes}" que apareceu '
+#     f'{qtd_apareceu_mais_vezes}x'
+# )
+
+linhas = 2
+colunas = 2
+ 
+linha = 1
+while linha <= linhas:
+    coluna = 1
+    while coluna <= colunas:
+        print(linha, coluna)
+        coluna += 1
+    linha += 1
